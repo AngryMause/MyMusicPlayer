7300b8740c56576373ff9caa042b9fb3e7ea6d66
AngryMause <chostblade@gmail.com>
2022-06-06 10:37:45 +0300

    make playMusicFrag

---
+++ MyMusicPlayer/app/src/main/java/com/example/mymusicplayer/data/entetis/Song.kt
@@ -0,0 +1,11 @@
+package com.example.mymusicplayer.data.entetis
+
+data class Song(
+    val mediaId: String? = "",
+    val imageUrl: String? = "",
+    val subtitle: String? = "",
+    val title: String? = "",
+    val songUrl: String? = ""
+)
+
+
